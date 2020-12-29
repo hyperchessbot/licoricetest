2d9a1ec423f373fb6531085a4507f97862b8f1f7
hyperchessbot <hyperchessbot@gmail.com>
2020-12-29 08:50:24 +0000

    Working example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use licorice::client::{Lichess};
+
+use tokio::stream::StreamExt;
+
+#[tokio::main]
+async fn main() {
+	let lichess = Lichess::default();
+
+	let query_params = vec![("max", "1")];
+
+	let mut stream = lichess
+		.export_all_games_json("chesshyperbot", Some(&query_params))
+		.await
+		.unwrap();
+
+	while let Some(game) = stream.next().await {
+    	let game = game.unwrap();
+    	println!("{:?}", game);
+	}
 }
