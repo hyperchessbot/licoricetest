e4fa002cf960c66ea6d2d907c3adf1e5a774add6
hyperchessbot <hyperchessbot@gmail.com>
2020-12-29 12:56:29 +0000

    Shakmaty test

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,31 @@ use tokio::stream::StreamExt;
 use dotenv::dotenv;
 use std::env;
 
+use shakmaty::{Chess, Position};
+use shakmaty::uci::Uci;
+use shakmaty::fen;
+
+fn _shakmaty(){
+	let pos = Chess::default();
+	
+	let legals = &pos.legals();
+	
+	println!("legals startpos = {:?}", legals.iter().map(|m| Uci::from_standard(&m).to_string()).collect::<Vec<String>>());
+	
+	match pos.play(&legals[0]) {
+		Ok(pos) => {
+			println!("legals startpos e4 = {:?}", &pos.legals().iter().map(|m| Uci::from_standard(&m).to_string()).collect::<Vec<String>>());
+			println!("fen = {:?}", fen::fen(&pos));
+		},
+		Err(err) => println!("{:?}", err)
+	}	
+}
+
 #[tokio::main]
 async fn main() {
 	dotenv().ok();
+	
+	//_shakmaty();
 
     for (key, value) in env::vars() {
 		match &key[..std::cmp::min(5, key.len())] {
