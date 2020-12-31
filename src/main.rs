ff35be5ff7858ca86cbbdbe784de1f346f326819
hyperchessbot <hyperchessbot@gmail.com>
2020-12-31 07:26:07 +0000

    Mongodb

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,46 @@ use tokio::stream::StreamExt;
 use dotenv::dotenv;
 use std::env;
 
-use shakmaty::{Chess, Position};
+use shakmaty::{Chess, Position, Move};
 use shakmaty::uci::{Uci, ParseUciError, IllegalUciError};
 use shakmaty::fen;
 
 use rand::prelude::*;
 
 use thiserror::Error;
+
+//////////////////////////////////////////////////////////////////
+// MongoDb
+use mongodb::{Client, options::ClientOptions};
+
+async fn _connect() -> Result<(), Box<dyn std::error::Error>>{
+	// Parse a connection string into an options struct.
+	let client_options = ClientOptions::parse(&std::env::var("RUST_MONGODB_URI").unwrap()).await?;
+
+	// Get a handle to the deployment.
+	let client = Client::with_options(client_options)?;
+
+	// List the names of the databases in that deployment.
+	for db_name in client.list_database_names(None, None).await? {
+		println!("{}", db_name);
+	}
+	
+	Ok(())
+}
+//////////////////////////////////////////////////////////////////
+
+/// Error when trying to play an illegal move.
+#[derive(Debug)]
+pub struct PlayError<'a, P> {
+    m: &'a Move,
+    inner: P,
+}
+
+impl<P> PlayError<'_, P> {
+    pub fn into_inner(self) -> P {
+        self.inner
+    }
+}
 
 #[derive(Error, Debug)]
 enum UciError {
@@ -30,7 +63,7 @@ fn _shakmaty_official() -> Result<(), Box<dyn std::error::Error>> {
 	Ok(())
 }
 
-fn make_uci_moves(ucis_str: &str) -> Result<String, Box<dyn std::error::Error>> {
+fn _make_uci_moves(ucis_str: &str) -> Result<String, Box<dyn std::error::Error>> {
 	let mut pos = Chess::default();
 	for uci_str in ucis_str.split(" ") {
 		let uci: Uci = uci_str.parse()?;						
@@ -83,7 +116,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 	
 	//_shakmaty();	
 	//let _ = _shakmaty_official();
-	println!("{}", make_uci_moves("e2e4 e7e5 g1f3")?);
+	//println!("{}", make_uci_moves("e2e4 e7e5 g1f3")?);
+	_connect().await?;
 
     for (key, value) in env::vars() {
 		match &key[..std::cmp::min(5, key.len())] {
